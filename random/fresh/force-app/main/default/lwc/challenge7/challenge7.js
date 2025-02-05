f6c738ee8fda3aa579204d956dc7fcb5de79224d
Andrew Yang <andrew.yang2022@gmail.com>
2025-02-05 11:30:27 -0600

    Finished RevPro LWC Challenges (Jest Test)

--- random/fresh/force-app/main/default/lwc/challenge7/challenge7.js
+++ random/fresh/force-app/main/default/lwc/challenge7/challenge7.js
@@ -1,14 +1,29 @@
-import { LightningElement, track } from 'lwc';
+import { api, LightningElement, track } from 'lwc';
 import getContacts from '@salesforce/apex/ContactHelper.getContacts';
+import getContact from '@salesforce/apex/ContactHelper.getContact';
 
 export default class Challenge7 extends LightningElement {
     @track
     contacts = [];
 
+    @track
+    contact;
+
+    @api
+    recordId
+
     handleClick(){
-       getContacts().
+    //    getContacts().
+    //    then((res)=> {
+    //     this.contacts = res;
+    //     contact = this.recordId;
+    //    }).catch((e) => {
+    //     console.log(e);
+    //    });
+
+       getContact({id:this.recordId}).
        then((res)=> {
-        this.contacts = res;
+        this.contact = res;
        }).catch((e) => {
         console.log(e);
        });
--- random/fresh/force-app/main/default/lwc/challengeEight/challengeEight.js
+++ random/fresh/force-app/main/default/lwc/challengeEight/challengeEight.js
@@ -1,8 +1,22 @@
-import { LightningElement,wire } from 'lwc';
+import { LightningElement,wire,api, track } from 'lwc';
 import getContacts from '@salesforce/apex/ContactHelper.getContacts';
+import getContact from '@salesforce/apex/ContactHelper.getContact';
 
 export default class ChallengeEight extends LightningElement {
-    @wire(getContacts)
-    contacts;
+    @track
+    contact;
+    error;
+    
+    @wire(getContact, {id: '$recordId'})
+    contactRetrieved({error, data}){
+        if(data) {
+            this.contact = data;
+        } else if (error) {
+            this.error = this.error;
+        }
+    }
+
+    @api 
+    recordId;
 
 }
